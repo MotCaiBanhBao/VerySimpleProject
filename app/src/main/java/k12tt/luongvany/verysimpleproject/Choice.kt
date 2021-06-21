fed204f7db13094d5a03494e4f3c523f8e638a19
MotCaiBanhBao <56585902+MotCaiBanhBao@users.noreply.github.com>
2021-06-21 10:23:35 +0700

    Update cấu trúc bài tập

---
+++ app/src/main/java/k12tt/luongvany/verysimpleproject/Choice.kt
@@ -0,0 +1,5 @@
+package k12tt.luongvany.verysimpleproject
+
+import android.content.Intent
+
+class Choice(val title: String, val description: String, val launchIntent: Intent)
---
+++ app/src/main/java/k12tt/luongvany/verysimpleproject/ChoiceAdapter.kt
@@ -0,0 +1,39 @@
+package k12tt.luongvany.verysimpleproject
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ChoiceAdapter(private val activity: Activity, private val choices: List<Choice>) :
+    RecyclerView.Adapter<ChoiceAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_choice, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val choice = choices[position]
+        holder.bind(choice)
+    }
+
+    override fun getItemCount(): Int {
+        return choices.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val titleText: TextView = itemView.findViewById(R.id.item_title)
+        private val descText: TextView = itemView.findViewById(R.id.item_description)
+        private val launchButton: Button = itemView.findViewById(R.id.item_launch_button)
+        fun bind(choice: Choice) {
+            titleText.text = choice.title
+            descText.text = choice.description
+            launchButton.setOnClickListener { activity.startActivity(choice.launchIntent) }
+        }
+
+    }
+}
